0d804f290267b4f0d7cd8fd4e070410d820a6231
Stan Bad <stankv@mail.ru>
2023-09-25 18:57:00 +0300

    Управление роботом-пылесосом. Авторизация.

---
+++ Lesson11/ERD/ERD.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk0`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk1`;
+
+ALTER TABLE `RobotAndUser` DROP FOREIGN KEY `RobotAndUser_fk0`;
+
+ALTER TABLE `RobotAndUser` DROP FOREIGN KEY `RobotAndUser_fk1`;
+
